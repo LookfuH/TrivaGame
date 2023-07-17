4c31d2a32d9b879a6bc023bb192e6f691580ba43
LookfuH <lookfu4@gmail.com>
2023-07-17 18:37:14 -0500

    adad

--- app/src/main/java/com/example/trivagame/MainActivity.kt
+++ app/src/main/java/com/example/trivagame/MainActivity.kt
@@ -6,13 +6,28 @@ import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity()
 {
+    var correct: Int = 0
+    var total: Int = 0
+    var A1 = Pair(true, "pick me")
+    var A2 = Pair(false, "not me")
+    var A3 = Pair(false, "not me")
+    var A4 = Pair(false, "not me")
+
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.startmanu)
 
     }
-    fun loadGame(v: View) = setContentView(R.layout.gamesession)
+    fun loadGame(v: View){
+        setContentView(R.layout.gamesession)
+
+    }
+    fun Gameplay(v: View){
+
+
+    }
 
     fun loadResult(v: View) = setContentView(R.layout.resultsmenu)
 
