a639dfad307ebe94ddf31f8f7e1fe0c5939ce254
Stephan Sommer <steven.sommer@go.winona.edu>
2023-07-30 01:21:38 -0500

    I got stuff working! :+1:

--- app/src/main/java/com/example/trivagame/MainActivity.kt
+++ app/src/main/java/com/example/trivagame/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.trivagame
 
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -9,9 +10,12 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class MainActivity : ComponentActivity()
-{
+class MainActivity : ComponentActivity() {
     private var correct: Int = 0
     private var total: Int = 0
 
@@ -21,9 +25,7 @@ class MainActivity : ComponentActivity()
     private lateinit var A3: Button
     private lateinit var A4: Button
 
-    //find a way to import the view from gamesession
-    private lateinit var GameView: View
-    //lateinit var GameView: View = findViewById(gameSession)
+    private lateinit var mediaPlayer: MediaPlayer
 
     data class Question(val text: String, val answers: List<String>, val correctAnswer: Int) {}
 
@@ -33,21 +35,23 @@ class MainActivity : ComponentActivity()
         Question(
             text = "What is Android Jetpack?",
             answers = listOf("All of these", "Tools", "Documentation", "Libraries"),
-            correctAnswer = 1),
+            correctAnswer = 1
+        ),
         Question(
             text = "What is the base class for layouts?",
             answers = listOf("ViewGroup", "ViewSet", "ViewCollection", "ViewRoot"),
-            correctAnswer = 1),
+            correctAnswer = 1
+        ),
         Question(
             text = "What layout do you use for complex screens?",
             answers = listOf("ConstraintLayout", "GridLayout", "LinearLayout", "FrameLayout"),
-            correctAnswer = 1),
+            correctAnswer = 1
+        ),
     )
 
     private var currentQuestionIndex = 0
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.startmanu)
 
@@ -85,7 +89,7 @@ class MainActivity : ComponentActivity()
     private fun Gameplay(view: View) {
 
         val question = questions[currentQuestionIndex]
-        val correctAnswerIndex = question.correctAnswer -1
+        val correctAnswerIndex = question.correctAnswer - 1
 
         val selectedAnswerIndex = when (view.id) {
             R.id.A1B -> 0
@@ -98,6 +102,8 @@ class MainActivity : ComponentActivity()
         val selectedAnswer = question.answers[selectedAnswerIndex]
 
         if (selectedAnswerIndex == correctAnswerIndex) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.its_true)
+            mediaPlayer.start()
             correct++
         }
         total++
@@ -107,33 +113,73 @@ class MainActivity : ComponentActivity()
             loadQuestion(questions[currentQuestionIndex])
         } else {
             loadResult()
+
         }
     }
 
-    fun loadResult() {
+    // Cleaned up to preventing memory leaks and improving resource management.
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
+
+    private fun loadResult() {
         setContentView(R.layout.resultsmenu)
         Toast.makeText(this, "Your score is $correct out of $total", Toast.LENGTH_SHORT).show()
-    }
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.how_did_that_make_you_feel)
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_startmanu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.menu_about -> showAboutDialog()
+        // Play the sound after a delay
+        val delayMillis = 1500L
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(delayMillis)
+            mediaPlayer.start()
         }
-        return super.onOptionsItemSelected(item)
-    }
 
-    private fun showAboutDialog() {
+        val retrybutton = findViewById<Button>(R.id.retrybutton)
+        retrybutton.setOnClickListener {
+            correct = 0
+            total = 0
+            currentQuestionIndex = 0
+
+
+            Toast.makeText(this, "Game is being reset. Good luck!", Toast.LENGTH_SHORT).show()
+
+            // Start of a new game
+            setContentView(R.layout.startmanu)
+
+            val startButton = findViewById<Button>(R.id.startsession)
+            startButton.setOnClickListener {
+                setContentView(R.layout.gamesession)
+                initializeGameSessionViews()
+                loadQuestion(questions[currentQuestionIndex])
+                Gameplay(it)
+            }
+        }
+
+
+        //For menu_startmanu.xml
+
+        //override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        //    menuInflater.inflate(R.menu.menu_startmanu, menu)
+        //     return true
+        //  }
+
+        //   override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //     when (item.itemId) {
+        //        R.id.menu_about -> showAboutDialog()
+        //     }
+        //     return super.onOptionsItemSelected(item)
+        // }
+
+        // private fun showAboutDialog() {
         //We can change this. This toast is just for testing.
-        showToast("This app is developed by Jordan Hamlett and Steven Sommer.")
-    }
+        //     showToast("This app is developed by Jordan Hamlett and Steven Sommer.")
+        // }
 
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        // private fun showToast(message: String) {
+        //    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        // }
     }
 }
