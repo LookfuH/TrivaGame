010ce80b8be384e7102948ba8e6cedcc93725f3f
Stephan Sommer <steven.sommer@go.winona.edu>
2023-08-01 17:05:07 -0500

    Added More Questions and just changed the correct answer to match answer

--- app/src/main/java/com/example/trivagame/MainActivity.kt
+++ app/src/main/java/com/example/trivagame/MainActivity.kt
@@ -32,19 +32,44 @@ class MainActivity : ComponentActivity() {
     private val questions: MutableList<Question> = mutableListOf(
 
         Question(
-            text = "What is Android Jetpack?",
-            answers = listOf("All of these", "Tools", "Documentation", "Libraries"),
+            text = "Which file extension is used to save Kotlin files?",
+            answers = listOf(".java", ".kot", ".kt or .kts", ".android"),
+            correctAnswer = 3
+        ),
+        Question(
+            text = "What is the default return type of any functions defined in Kotlin?",
+            answers = listOf("Int", "DoubleCatfish", "Void", "Unit"),
+            correctAnswer = 4
+        ),
+        Question(
+            text = "Which keyword is used to indicate that a class can be subclassed?",
+            answers = listOf("public", "open", "internal", "protected"),
+            correctAnswer = 2
+        ),
+        Question(
+            text = "Which Gradle configuration indicates the most recent API level your app has been tested with?",
+            answers = listOf("minSdkVersion", "compileSdkVersion", "targetSdkVersion", "testSdkVersion"),
+            correctAnswer = 3
+        ),
+        Question(
+            text = "What is EditText a subclass of?",
+            answers = listOf("TextField", "LinearLayout", "TextView", "Button"),
+            correctAnswer = 3
+        ),
+        Question(
+            text = "Which lifecycle method is called to give an activity focus?",
+            answers = listOf("onResume()", "onVisible()", "Catfish", "onFocus()"),
             correctAnswer = 1
         ),
         Question(
-            text = "What is the base class for layouts?",
-            answers = listOf("ViewGroup", "ViewSet", "ViewCollection", "ViewRoot"),
-            correctAnswer = 1
+            text = "What was Collin's minor as an undergrad?",
+            answers = listOf("Catfish", "French", "German", "Math"),
+            correctAnswer = 2
         ),
         Question(
-            text = "What layout do you use for complex screens?",
-            answers = listOf("ConstraintLayout", "GridLayout", "LinearLayout", "FrameLayout"),
-            correctAnswer = 1
+            text = "When did Collin started Teaching at WSU",
+            answers = listOf("2012", "1974", "2017", "2021"),
+            correctAnswer = 4
         ),
     )
 
@@ -146,7 +171,7 @@ class MainActivity : ComponentActivity() {
             currentQuestionIndex = 0
 
 
-            Toast.makeText(this, "Game is being reset. Good luck!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Game is reset. Good luck!", Toast.LENGTH_SHORT).show()
 
             // Start of a new game
             setContentView(R.layout.startmanu)
